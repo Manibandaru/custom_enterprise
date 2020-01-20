14a8f17aa53d918d2fba7d7156bda626fd3747e9
Manishankar <shankar.bandaru777@gmail.com>
2020-01-20 19:22:58 +0530

    skydest badge

---
+++ skydest/__manifest__.py
@@ -0,0 +1,24 @@
+
+{
+    'name': 'Badge View report',
+    'version': '1.0',
+    'website' : 'https://www.sidmectech.com',
+    'category': 'Base',
+    'summary': 'Badge view for skydest event',
+    'description': """
+
+""",
+    'author': 'Mani Shankar',
+    'depends': ['base','web','event'],
+    'data': [
+
+        'views/badge_view.xml',
+
+
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
