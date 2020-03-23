06ae6ea952df1f749ec497a487ea2e2b26f4538e
Manishankar <shankar.bandaru777@gmail.com>
2020-03-23 19:04:24 +0530

    changes for tracking job with container and bl number along with reports

--- logistics_addons/models/custom_sale_order.py
+++ logistics_addons/models/custom_sale_order.py
@@ -1,12 +1,15 @@
 
 from odoo import fields, models,api,_
 from odoo.fields import Date
+from odoo.exceptions import UserError, ValidationError
 
 class SaleOrder(models.Model):
 	_inherit = "sale.order"
 
 
 	job_type = fields.Many2one('so.job.type', string='Job Type')
+	client_order_ref = fields.Char(string='BL Number', copy=False )
+	container_no = fields.Char(string='Container Number')
 
 	@api.model
 	def create(self, vals):
@@ -82,6 +85,43 @@ class SaleOrder(models.Model):
 	# 		return sale_orders.action_view_invoice()
 	# 	return {'type': 'ir.actions.act_window_close'}
 	#
+
+	@api.multi
+	def _prepare_invoice(self):
+		"""
+        Prepare the dict of values to create the new invoice for a sales order. This method may be
+        overridden to implement custom invoice generation (making sure to call super() to establish
+        a clean extension chain).
+        """
+		self.ensure_one()
+		company_id = self.company_id.id
+		journal_id = (self.env['account.invoice'].with_context(company_id=company_id or self.env.user.company_id.id)
+			.default_get(['journal_id'])['journal_id'])
+		if not journal_id:
+			raise UserError(_('Please define an accounting sales journal for this company.'))
+		invoice_vals = {
+			'name': self.client_order_ref or '',
+			'origin': self.name,
+			'type': 'out_invoice',
+			'account_id': self.partner_invoice_id.property_account_receivable_id.id,
+			'partner_id': self.partner_invoice_id.id,
+			'partner_shipping_id': self.partner_shipping_id.id,
+			'journal_id': journal_id,
+			'currency_id': self.pricelist_id.currency_id.id,
+			'comment': self.note,
+			'payment_term_id': self.payment_term_id.id,
+			'fiscal_position_id': self.fiscal_position_id.id or self.partner_invoice_id.property_account_position_id.id,
+			'company_id': company_id,
+			'user_id': self.user_id and self.user_id.id,
+			'team_id': self.team_id.id,
+			'transaction_ids': [(6, 0, self.transaction_ids.ids)],
+			'bl_number': self.client_order_ref,
+			'container_no': self.container_no,
+			'job_number':self.id,
+
+		}
+		return invoice_vals
+
 
 class JobType_so(models.Model):
 	_name = 'so.job.type'
